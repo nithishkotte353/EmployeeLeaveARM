2e2612ca4af8e9871e8cc3f4ba05f6e835e53cfd
rajeswarrao penchala <raj.penchala.azure@gmail.com>
2018-06-10 18:16:04 -0400

    The Employee Leave

---
+++ EmployeeLeaveArm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeLeaveArm.MvcApplication" Language="C#" %>
---
+++ EmployeeLeaveArm/Models/EmployeeLeave.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeLeaveArm.Models
+{
+    public class EmployeeLeave
+    {
+       public string EmployeeNumber { get; set; }
+       public string EmployeeName { get; set; }
+       public int Days { get; set; }
+       public string Reason { get; set; }
+    }
+}
